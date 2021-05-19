5a3a46ed49d0e7660052ee77efd1048ff434dddb
sanand34 <sanchitanand34@gmail.com>
2021-05-20 00:28:49 +0530

    initial commit

---
+++ truffle-config.js
@@ -0,0 +1,34 @@
+const HTTPProviderRateLimitRetry = require('./lib/http-provider-rate-limit-retry')
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      provider: () => {
+        const appCred = ''; // from application credential widget
+        const connectionURL = ''; // without protocol (https://)        
+        return new HTTPProviderRateLimitRetry(`https://${appCred}@${connectionURL}`, 100000);
+      },
+      network_id: "*", // Match any network id
+      gasPrice: 0,
+      gas: 4500000,
+      disableConfirmationListener: true, // generates thousands of eth_getBlockByNumber calls
+      timeoutBlocks: 3,
+      deploymentPollingInterval: 5000,
+      /* type: 'quorum' // Use this property for Quorum environments */
+    },
+  },
+  mocha: {
+    enableTimeouts: false,
+    before_timeout: 600000
+  },
+  compilers: {
+    solc: {
+      version: "0.7.0",
+      settings: {
+        evmVersion: "constantinople"
+      }
+    },
+  }
+};
